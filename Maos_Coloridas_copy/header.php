59078d19ef0acfc4101c7ccfe007c607397d39be
Matheus Brugge Milczwski <128723144+Brugge-Matheus@users.noreply.github.com>
2024-04-15 19:04:37 -0300

    firts commit in staging

--- Maos_Coloridas_copy/header.php
+++ Maos_Coloridas_copy/header.php
@@ -1,3 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Teste</title>
+    <link rel="stylesheet" href="./css/./home.css">
+</head>
+</html>
 <header class="header">
         <div class="header-container container">
             <a href="home.html" class="header-logo">
--- Maos_Coloridas_copy/home.php
+++ Maos_Coloridas_copy/home.php
@@ -1,13 +1,4 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Mão Colorida</title>
-    <link rel="stylesheet" href="../Maos_Coloridas\css\home.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <link rel="shortcut icon" href="imagens/mao_colorida_favicon.jpeg" type="image/x-icon">
-</head>
+<?php include 'header.php'?>
 <body>
     <?php include 'header.php';?>
 
