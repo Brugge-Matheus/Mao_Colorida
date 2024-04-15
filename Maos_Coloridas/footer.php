b5c2deb30ae50f865a72153b6dea7e8f60053e8d
Matheus Brugge Milczwski <128723144+Brugge-Matheus@users.noreply.github.com>
2024-04-15 20:11:12 -0300

    staging changes

---
+++ Maos_Coloridas/footer.php
@@ -0,0 +1,37 @@
+<footer>
+        <div class="container">
+        <div class="footer-logo">
+        <picture href="">
+            <img src="imagens/logo-header.png">
+        </picture>
+          <a href="">Rua João Lunardelli, 80 - Cidade Industrial,<br> Curitiba - PR, 81460-100</a>
+          <a href=""> (41)3059-4800</a>
+        </div>
+
+        <div class="footer-main">
+
+          <div class="footer-column">
+            <span>Menu</span>
+            <a href="">Início</a>
+            <a href="">Quem somos</a>
+            <a href="">Contatos</a>
+            <a href="">Blog</a>
+            <a href="">Trabalhe conosco</a>
+          </div>
+
+          <div class="footer-column">
+            <span>Categorias</span>
+            <a href="produtos/marcas">Marcas</a>
+            <a href="segmentos">Segmentos</a>
+          </div>
+
+          <div class="footer-column">
+            <span>Redes sociais</span>
+            <a href="">Linkedin</a>
+            <a href="">Instagram</a>
+            <a href="">Facebook</a>
+          </div>
+
+        </div>
+      </div>
+    </footer>
--- Maos_Coloridas/home.php
+++ Maos_Coloridas/home.php
@@ -1,16 +1,3 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Mão Colorida</title>
-    <link rel="stylesheet" href="css/home.css">
-    <link rel="stylesheet" href="css/sustentabilidade.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer"/>
-    <link rel="shortcut icon" href="imagens/mao_colorida_favicon.jpeg" type="image/x-icon">
-
-</head>
-<body>
     <header class="header">
         <div class="header-container container">
             <a href="home.html" class="header-logo">
@@ -18,9 +5,9 @@
             </a>
         
             <nav class="header-menu">
-                <a href="#" class="header-link">Quem somos</a>
+                <a href="#nossa-historia" class="header-link">Quem somos</a>
                 <a href="#" class="header-link">Benefícios</a>
-                <a href="#" class="header-link">Seguranças </a>
+                <a href="#seguranca-patrimonial" class="header-link">Seguranças </a>
                 <a href="#" class="header-link">Blog</a>
                 <a href="#" class="fale-conosco-btn">
                     <i class="fa-solid fa-paper-plane"></i>
@@ -31,42 +18,130 @@
     </header>
 
     <!-- <div class="menu">
-      <nav class="container">
-          <div class="menu-drop">
-              <i class="fa-solid fa-bars"></i>
-              <a href="#">Categorias</a>
-              <i class="fa-solid fa-chevron-down"></i>
-          </div>
-      <div class="menu-item">
-          <a href="#">Linha leve</a>
-      </div>
-      |
-      <div class="menu-item">
-          <a href="#">Linha pesada</a>
-      </div>
-      |
-      <div class="menu-item">
-          <a href="#">Motos</a>
-      </div>
-      |
-      <div class="menu-item">
-          <a href="#">Estética e Manutenção</a>
-      </div>
-      |
-      <div class="menu-item">
-          <a href="#">Pneus</a>
-      </div>
-      |
-      <div class="menu-item">
-          <a href="#">Segmentos</a>
-      </div> 
-          </div>
-      </nav> 
-      
-  </div> -->
+        <nav class="container">
+            <div class="menu-drop">
+                <i class="fa-solid fa-bars"></i>
+                <a href="#">Categorias</a>
+                <i class="fa-solid fa-chevron-down"></i>
+            </div>
+        <div class="menu-item">
+            <a href="#">Linha leve</a>
+        </div>
+        |
+        <div class="menu-item">
+            <a href="#">Linha pesada</a>
+        </div>
+        |
+        <div class="menu-item">
+            <a href="#">Motos</a>
+        </div>
+        |
+        <div class="menu-item">
+            <a href="#">Estética e Manutenção</a>
+        </div>
+        |
+        <div class="menu-item">
+            <a href="#">Pneus</a>
+        </div>
+        |
+        <div class="menu-item">
+            <a href="#">Segmentos</a>
+        </div> 
+            </div>
+        </nav>
+        
+    </div> -->
 
-<!-- Segurança Patrimonial -->
-<div class="titulo-segurança-pat d-flex">
+    <!-- Nossa história -->
+    <section class="nossa-historia" id="nossa-historia">
+        <div class="container-historia container">
+            <div class="hist-img">
+                <img src="imagens/sobre-img.jpg" alt="sobre_nos_image">
+            </div>
+            <div class="hist-text">
+                <h2>Nossa história</h2>
+                <p>
+                    Fundada em 1997 em Curitiba por Marcos Vargas, após retornar da Espanha, onde adquiriu experiência em comunicação visual e sinalização. A empresa Mão Colorida tem como objetivo de trabalho projetar Ponto de Vendas (PDV) para as empresas conforme a necessidade, integrando criação, engenharia, produção, atendimento comercial e pós venda.
+                    <br>
+                    <br>
+                    Alguns dos projetos arquitetados foram para as empresas O Boticário, Adidas, Cacau Show, Samsung, etc. De maneira completamente sustentável a empresa desenvolve designs tecnológicos, modernos e inovadores.
+                    Um desses projetos foi para a marca Quem Disse Berenice? O qual apresenta muita cor e modernidade que definem perfeitamente a personalidade da marca. Além disso todos os revestimentos da loja foram confeccionados com plástico reciclado, desde a fachada ate os móveis.
+                    </p>
+            </div>
+        </div>
+    </section>
+
+
+    <!-- sustentabilidade e foco -->
+    <section class="processos d-flex" id="nosso-foco">
+        <div class="processos-container d-flex">
+            <div class="processos-conteudo d-flex">
+                <div class="container d-flex">
+                    <div class="processos-text d-flex">
+                        <h3>SUSTENTABILIDADE EM FOCO</h3>
+                        <p>
+                          Orientar as questões socioambientais associadas aos seguimentos da <strong>Mão Colorida</strong>, representa o comprometimento da empresa com o meio ambiente, buscando a melhoria da qualidade de vida dos seus empregados e da comunidade em geral. Reforçando assim o compromisso com o desenvolvimento sustentável do planeta. 
+                          <br>
+                          <br>
+                          A PNRS- LEI Nº 12.305 de 02 de agosto de 2010 que institui a Politica nacional de resíduos sólidos e a lei que rege o setor de resíduos sólidos e define a ordem de prioridade no gerenciamento da seguinte maneira:
+  
+                        </p>
+                        <div class="processos-topicos d-flex">
+                            <div class="topico1 d-flex">
+                              <span><i class="fas fa-check laranja"></i> Não geração</span>
+                              <span><i class="fas fa-check laranja"></i> Redução</span>
+                              <span><i class="fas fa-check laranja"></i> Reutilização</span>
+                            </div>
+                            <div class="topico2 d-flex">
+                              <span><i class="fas fa-check laranja"></i> Reciclagem</span>
+                              <span><i class="fas fa-check laranja"></i> Tratamento </span>
+                              <span><i class="fas fa-check laranja"></i> Disposiçao adequada</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="processos-img d-flex">
+                        <img src="imagens/processos-img.jpg" alt="imagem">
+                    </div>
+                </div>
+            </div>
+  
+  
+  </section>
+
+
+  <!-- nossos objetivos -->
+    <section class="objetivos" id="objetivos">
+        <div class="container">
+            <div class="objetivos-titulo">
+                <h2>
+                    <i class="fa-solid fa-bullseye"></i>
+                    Pilares Fundamentais
+                </h2>
+                
+            </div>
+
+            <div class="box-objetivos">
+                <div class="itens-objetivos box1">
+                    <img src="imagens/icone-missão.png" alt="">
+                    <h2>Missão</h2>
+                    <p>Utilizar nossa criatividade e as melhores tecnologias do mercado para tornar realidade os sonhos e objetivos dos clientes</p>
+                </div>
+                <div class="itens-objetivos box2">
+                    <img src="imagens/icone-visao.png" alt="">
+                    <h2>Visão</h2>
+                    <p>Ser uma empresa de vanguarda atendendo os clientes mais exigentes do mercado com uma equipe motivada por resultados</p>
+                </div>
+                <div class="itens-objetivos box3">
+                    <img src="imagens/icone-coracao.png" alt="">
+                    <h2>Valores</h2>
+                    <p>A qualidade superior que entregamos ao nossos clientes junto com a busca da rentabilidade e do crescimento do nosso negocio</p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- Segurança Patrimonial -->
+<div class="titulo-segurança-pat d-flex" id="seguranca-patrimonial">
     <span class="span-titulo">Responsabilidade</span>
     <h2>Segurança Patrimonial</h2>
 </div>
@@ -137,7 +212,7 @@
 
 
 <!-- Segurança Do Trabalho -->
-<section class="processos d-flex">
+<section class="processos d-flex" id="segurança-do-trabalho">
     <div class="titulo-processos d-flex">
         <span class="span-titulo">Trabalho Seguro</span>
         <h2>Segurança do Trabalho</h2>
@@ -272,47 +347,6 @@
     </div>
   </div>
 </section>
-
-
-    <footer>
-        <div class="container">
-        <div class="footer-logo">
-        <picture href="">
-            <img src="imagens/logo-header.png">
-        </picture>
-          <a href="">Rua João Lunardelli, 80 - Cidade Industrial,<br> Curitiba - PR, 81460-100</a>
-          <a href=""> (41)3059-4800</a>
-        </div>
-
-        <div class="footer-main">
-
-          <div class="footer-column">
-            <span>Menu</span>
-            <a href="">Início</a>
-            <a href="">Quem somos</a>
-            <a href="">Contatos</a>
-            <a href="">Blog</a>
-            <a href="">Trabalhe conosco</a>
-          </div>
-
-          <div class="footer-column">
-            <span>Categorias</span>
-            <a href="produtos/marcas">Marcas</a>
-            <a href="segmentos">Segmentos</a>
-          </div>
-
-          <div class="footer-column">
-            <span>Redes sociais</span>
-            <a href="">Linkedin</a>
-            <a href="">Instagram</a>
-            <a href="">Facebook</a>
-          </div>
-
-        </div>
-      </div>
-    </footer>
     
 
     <script src="js/main.js"></script>
-</body>
-</html>
