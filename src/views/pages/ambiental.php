da4a234891e9aa66217d612730b6c8381e37fec5
Brugge-Matheus <128723144+Brugge-Matheus@users.noreply.github.com>
2024-06-12 00:27:56 -0300

    changes

--- src/views/pages/ambiental.php
+++ src/views/pages/ambiental.php
@@ -9,15 +9,14 @@
         <div class="menu-item">
             <a href="#medidas">Nossas medidas</a>
         </div>
-        <!-- <div class="menu-item">
-            <a href="#nosso-foco">Nossos esforços</a>
-        </div>
+        |
         <div class="menu-item">
-            <a href="#objetivos">Missão e valores</a>
+            <a href="#plano">Plano de gerenciamento</a>
         </div>
+        |
         <div class="menu-item">
-            <a href="#medidas">Medidas</a>
-        </div> -->
+            <a href="#disposicao">Disposição de resíduos</a>
+        </div>
     </nav>
 </div>
 
@@ -35,7 +34,7 @@
                         assim o compromisso com o desenvolvimento sustentável do planeta.
                         <br>
                         <br>
-                        A PNRS- LEI Nº 12.305 de 02 de agosto de 2010 que institui a Politica nacional de resíduos
+                        A disposicao- LEI Nº 12.305 de 02 de agosto de 2010 que institui a Politica nacional de resíduos
                         sólidos e a lei que rege o setor de resíduos sólidos e define a ordem de prioridade no
                         gerenciamento da seguinte maneira:
 
@@ -127,6 +126,90 @@
         <p id="modal-text"></p>
     </div>
 </div>
+
+
+
+
+<!-- Plano de gerenciamento -->
+<section class="plano d-flex" id="plano">
+    <div class="plano-container d-flex">
+        <div class="plano-conteudo d-flex">
+            <div class="container d-flex">
+                <div class="plano-text d-flex">
+                    <h3>PLANO DE GERENCIAMENTO DE RESÍDUOS SÓLIDOS</h3>
+                    <p>
+                        O Plano de Gerenciamento de Resíduos Sólidos (PGRS) é um documento técnico essencial para a gestão ambiental de empresas. Ele identifica a tipologia e a quantidade de resíduos gerados, comprovando a capacidade da empresa de gerir esses resíduos de maneira ambientalmente correta.
+                        <br>
+                        <br>
+                        O PGRS orienta sobre as formas adequadas para o manejo, armazenamento, transporte, reciclagem e destinação final dos resíduos, contribuindo significativamente para a redução e prevenção de grandes poluições ambientais, bem como suas consequências para a saúde pública e o equilíbrio da fauna e flora.
+                    </p>
+                    <div class="plano-topicos d-flex">
+                        <div class="topico1 d-flex">
+                            <h4>Identificação de Resíduos</h4>
+                            <span><i class="fa-solid fa-arrow-right"></i> Tipologia e quantidade de resíduos gerados.</span>
+                            <!-- </div> -->
+                            <!-- <br> -->
+                            <!-- <div class="topico2 d-flex"> -->
+                            <h4>Gestão Ambiental</h4>
+                            <span><i class="fa-solid fa-arrow-right"></i> Comprova a gestão correta dos resíduos.</span>
+                            <!-- <br> -->
+                            <h4>Manejo e Destinação</h4>
+                            <span><i class="fa-solid fa-arrow-right"></i> Manejo, armazenamento, transporte, reciclagem e destinação final adequados.</span>
+
+                            <h4>Prevenção de Poluição</h4>
+                            <span><i class="fa-solid fa-arrow-right"></i> Reduz e evita poluições ambientais e suas consequências.</span>
+
+                            <h4>Legislação</h4>
+                            <span><i class="fa-solid fa-arrow-right"></i> <strong>Artigo 3:</strong> Obrigações quanto ao PGRS.</span>
+                            <span><i class="fa-solid fa-arrow-right"></i> <strong>Artigo 25:</strong> Multa por descumprimento.</span>
+                            <span><i class="fa-solid fa-arrow-right"></i> <strong>Artigo 33:</strong> Requisito para renovação da Licença.</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="plano-img d-flex">
+                    <img src="<?=$base?>/assets/images/pgrs.png" alt="imagem">
+                </div>
+            </div>
+        </div>
+</section>
+
+
+
+
+
+<!-- disposicao -->
+<section class="disposicao d-flex" id="disposicao">
+    <div class="disposicao-container d-flex">
+        <div class="disposicao-conteudo d-flex">
+            <div class="container d-flex">
+            <div class="disposicao-img d-flex">
+                    <img src="<?=$base?>/assets/images/disposicao-de-residuos.jpg" alt="imagem">
+                </div>
+                <div class="disposicao-text d-flex">
+                    <h3>DISPOSIÇÃO FINAL DOS RESÍDUOS</h3>
+                    <p>
+                        Atualmente, uma das principais preocupações das empresas é garantir a destinação correta dos resíduos gerados em seus processos. Esta ação é fundamental para a Política Ambiental da empresa e para o seu sistema de gestão ambiental. 
+                        <br>
+                        <br>
+                        A destinação adequada dos resíduos não só cumpre com as exigências legais e ambientais, mas também demonstra o compromisso da empresa com a sustentabilidade. 
+
+                    </p>
+                    <div class="disposicao-topicos d-flex">
+                        <div class="topico1 d-flex">
+                            <span><i class="fa-solid fa-star"></i> Co-processamento;</span>
+                            <span><i class="fa-solid fa-star"></i> Tratabilidade</span>
+                            <span><i class="fa-solid fa-star"></i> Reciclagem</span>
+                        </div>
+                        <div class="topico2 d-flex">
+                            <span><i class="fa-solid fa-star"></i> Recuperação</span>
+                            <span><i class="fa-solid fa-star"></i> Disposição </span>
+                        </div>
+                    </div>
+                </div>
+                
+            </div>
+        </div>
+</section>
 
 
 
